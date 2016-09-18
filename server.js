f820b9ed400ac12293fa33796b8f5f2a34f54a0c
melvin2016 <melvingeorge10@gmail.com>
2016-09-19 00:31:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,17 +8,8 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
-
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
-
 app.get('/profile', function (req, res) {
-  res.send('Profile Of Meak Inc.');
+  res.sendFile(path.join(__dirname, 'ui', 'profile.html'));
 });
 
 app.get('/works', function (req, res) {
@@ -27,6 +18,14 @@ app.get('/works', function (req, res) {
 
 app.get('/vision', function (req, res) {
   res.send('Vision Of Meak Inc.');
+});
+
+app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+
+app.get('/ui/madi.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
