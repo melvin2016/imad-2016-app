f3fa5f9472a4f05dd15ba948d6318b4d5c1cab3f
melvin2016 <melvingeorge10@gmail.com>
2016-09-27 00:27:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 var meak = document.getElementById("meak");
 var content = document.getElementById("myCont");
 content = innerHTML("Hey This Is Meak Studios ! With Motions And all :-/ ");
-meak.onClick(function(){
+meak.onclick(function(){
     meak.style.marginLeft="100px";
     
 });
