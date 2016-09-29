aaddba1aa35e36db3059269ef2a497ada1707326
melvin2016 <melvingeorge10@gmail.com>
2016-09-29 23:02:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,9 +3,21 @@ var button = document.getElementById('counter');
 var counter=0;
 button.onclick = function(){
     
-    counter = counter + 1;
-    span = document.getElementById('count');
-    span.innerHTML = counter.toString();
-    
-    
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function(){
+        
+        if(request.readyState===XMLHttpRequest.DONE){
+            //We should do something
+            if(request.status===200){
+                var counter = request.responseText;
+                span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+                
+            }
+            
+        }
+        
+    };
+    request.open('GET','http://melvin2016.imad.hasura-app.io/counter',true);
+    request.send(null);
 };
