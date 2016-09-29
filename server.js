4a18b966462ab676f03b038ddc597a59c659de6e
melvin2016 <melvingeorge10@gmail.com>
2016-09-29 21:32:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -83,18 +83,20 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var counter=0;
+app.get('/counter', function(req,res){
+   counter = counter + 1;
+   res.send(counter.toString());
+    
+});
+
 //For Other PAges
 app.get('/:articlename', function (req, res) {
     var articleName = req.params.articlename;
     res.send(createTemplate(articles[articleName]));
 });
 
-var counter=0;
-app.get('/counter',function (req,res){
-   counter = counter + 1;
-   res.send(counter.toString());
-    
-});
+
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
