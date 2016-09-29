a075c50b1386386555d317ca3205431e00a33aea
melvin2016 <melvingeorge10@gmail.com>
2016-09-29 21:54:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,11 @@
 //counter variable and function 
 var button = document.getElementById('counter');
+var counter=0;
 button.onClick = function(){
-    var counter=0
+    
     counter = counter + 1;
+    span = document.getElementById('noOftimes');
+    span.innerHTML = counter.toString();
     
     
 };
