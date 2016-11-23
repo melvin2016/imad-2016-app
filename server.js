c95ba4e28f9b98caf83320e6e28572069935df04
melvin2016 <melvingeorge10@gmail.com>
2016-11-23 20:58:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var morgan = require('morgan');
+var Pool = require('pg').Pool;
 var path = require('path');
+var config = {
+  host : 'localhost',
+  user : 'postgres',
+  password : '',
+  port : '5432',
+  database : 'test'
 
+};
+var pool = new Pool(config);
 var app = express();
 app.use(morgan('combined'));
+app.get('/test-db',function(req,res){
+  pool.query('SELECT * FROM test ',function(err,result){
+    if(err){
+      res.status(500).send(err.toString());
+
+    }else{
+  res.send(JSON.stringify(result));
+}
+  })
+
+});
+
 
 var articles={
     profile:{
@@ -11,7 +32,7 @@ var articles={
     head_img:"me.jpg",
     heading:"Profile",
     para1:"Name : Melvin George (Chatan)",
-    para2:"<span style=color:#a7ea09>Current Status :  Developing Imad App ! :D</span>",
+    para2:"<span style=color:#a7ea09>Current Status :  Developing Imad App ! :D</span>"
 
 },
     works:{
@@ -19,14 +40,14 @@ var articles={
         head_img:"works.png",
         heading:"The Works",
         para1:"Coming Soon !",
-        para2:" Just Kidding ! ",
+        para2:" Just Kidding ! "
         },
     vision:{
             title:"Vision",
             head_img:"vision.png",
             heading:"VISION !",
             para1:"<bold>If today were the last day Of your Life,Would you want to do what you are about to do today ?<br><i> - Steve Jobs</i></bold>",
-            para2:"<bold>Thanks to IMAD Team for Helping Me Build This !</bold>",
+            para2:"<bold>Thanks to IMAD Team for Helping Me Build This !</bold>"
         }
 }
 
@@ -52,66 +73,68 @@ function createTemplate(data){
     var para2 = data.para2;
 
     var htmlContent=`<html>
-    <head>
-        <title>
-            ${title}
-        </title>
-        <link href="https://fonts.googleapis.com/css?family=Amatic+SC|Baloo+Bhai|Lalezar|Lato|Oswald" rel="stylesheet">
-        <link type="text/css"href="/ui/style.css" rel="stylesheet" />
-        <meta name="viewport" content="width=device-width, initial-sacle=1">
+<head>
+    <title>
+        ${title}
+    </title>
+    <link href="https://fonts.googleapis.com/css?family=Amatic+SC|Baloo+Bhai|Lalezar|Lato|Oswald" rel="stylesheet">
+    <link type="text/css"href="/ui/style.css" rel="stylesheet" />
+    <meta name="viewport" content="width=device-width, initial-sacle=1">
 
-    </head>
-    <body>
+</head>
+<body>
 
 
-        <div class="container">
-            <div class="links">
-              <ul>
-                <li><a class="active" href="/"><img src="https://cdn2.iconfinder.com/data/icons/windows-8-metro-style/512/home.png" alt="Home"> Home</a></li>
-                <li><a href="profile"><img src="https://4bis.nl/img/profile.png" alt="Profile"> Profile</a></li>
-                <li><a href="works"><img src="http://intu.co.uk/uploads/media/thumbnail/0001/19/thumb_18979_thumbnail_1x.png" alt="Works"> The Works</a></li>
-                <li ><a  href="vision"><img src="http://www.pngall.com/wp-content/uploads/2016/05/Vision-Free-Download-PNG.png" alt="vision">VISION</a></li>
-              </ul>
-            </div>
-            <div class="center">
-                <img src="/ui/${head_img}" class="img-medium"/>
-            </div>
-            <h1>${heading}<hr></h1>
-
-            <p>
-                ${para1}
-            </p>
-            <p>
-                ${para2}
-            </p>
-
-            <fieldset>
-                    <legend>
-                    Your Opinions !
-                    </legend>
-                    <div style="color:#dd4b39"><button id="counter">Like <3 !</button> <span id="count"> 0 </span>&nbsp;Likes !</div><br>
-                    Your Comments here !&nbsp; : &nbsp;&nbsp;<input type="text" Placeholder="Enter Comment Here !" id="input_comment">
-                    &nbsp;&nbsp;&nbsp;<input type="submit" value="Submit !" id="submit_btn">&nbsp;&nbsp;&nbsp;
-
-                    <ul id="ul_list">
-
-                    </ul>
-            </fieldset>
+    <div class="container">
+        <div class="links">
+          <ul>
+            <li><a class="active" href="/"><img src="https://cdn2.iconfinder.com/data/icons/windows-8-metro-style/512/home.png" alt="Home"> Home</a></li>
+            <li><a href="profile"><img src="https://4bis.nl/img/profile.png" alt="Profile"> Profile</a></li>
+            <li><a href="works"><img src="http://intu.co.uk/uploads/media/thumbnail/0001/19/thumb_18979_thumbnail_1x.png" alt="Works"> The Works</a></li>
+            <li ><a  href="vision"><img src="http://www.pngall.com/wp-content/uploads/2016/05/Vision-Free-Download-PNG.png" alt="vision">VISION</a></li>
+          </ul>
         </div>
+        <div class="center">
+            <img src="/ui/${head_img}" class="img-medium"/>
+        </div>
+        <h1>${heading}<hr></h1>
 
-      <footer>
-      <p>
-        <a href="https://twitter.com/melvingeorge11"><img src="http://icons.iconarchive.com/icons/spoon-graphics/doodle/128/Twitter-icon.png"></a>
-        &nbsp&nbsp&nbsp<a href="https://www.facebook.com/alkitj10"><img src="http://inc.anphicle.com/2012/01/facebook.png"></a>
-      </p>
-      &copy Meak Inc.
-      </footer>
-      <script type="text/javascript" src="/ui/main.js"> </script>
-    </body>
+        <p>
+            ${para1}
+        </p>
+        <p>
+            ${para2}
+        </p>
+
+        <fieldset>
+                <legend>
+                Your Opinions !
+                </legend>
+                <div style="color:#dd4b39"><button id="counter">Like <3 !</button> <span id="count"> 0 </span>&nbsp;Likes !</div><br>
+                Your Comments here !&nbsp; : &nbsp;&nbsp;<input type="text" Placeholder="Enter Comment Here !" id="input_comment">
+                &nbsp;&nbsp;&nbsp;<input type="submit" value="Submit !" id="submit_btn">&nbsp;&nbsp;&nbsp;
+
+                <ul id="ul_list">
+
+                </ul>
+        </fieldset>
+    </div>
+
+  <footer>
+  <p>
+    <a href="https://twitter.com/melvingeorge11"><img src="http://icons.iconarchive.com/icons/spoon-graphics/doodle/128/Twitter-icon.png"></a>
+    &nbsp&nbsp&nbsp<a href="https://www.facebook.com/alkitj10"><img src="http://inc.anphicle.com/2012/01/facebook.png"></a>
+  </p>
+  &copy Meak Inc.
+  </footer>
+  <script type="text/javascript" src="/ui/main.js"> </script>
+</body>
 </html>`
 
 return htmlContent;
 }
+
+
 
 //For Home Page
 app.get('/', function (req, res) {
@@ -121,7 +144,7 @@ app.get('/', function (req, res) {
 
 
 //For Other PAges
-app.get('/:articlename', function (req, res) {
+app.get('/articles/:articlename', function (req, res) {
     var articleName = req.params.articlename;
     res.send(createTemplate(articles[articleName]));
 });
