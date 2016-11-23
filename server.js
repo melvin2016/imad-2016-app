b6b844ad10b0357d8d2e059be1465d393fae45e5
melvin2016 <melvingeorge10@gmail.com>
2016-11-23 22:36:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,17 +13,6 @@ var config = {
 var pool = new Pool(config);
 var app = express();
 app.use(morgan('combined'));
-app.get('/test-db',function(req,res){
-  pool.query('SELECT * FROM test ',function(err,result){
-    if(err){
-      res.status(500).send(err.toString());
-
-    }else{
-  res.send(JSON.stringify(result));
-}
-  })
-
-});
 
 
 var articles={
