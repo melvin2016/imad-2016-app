d4bb51ee3862fac7828b28d39e2f3990216d8828
melvin2016 <melvingeorge10@gmail.com>
2016-11-23 22:30:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -145,6 +145,20 @@ app.get('/', function (req, res) {
 
 //For Other PAges
 app.get('/articles/:articlename', function (req, res) {
+    pool.query("SELECY * FROM articles WHERE title =' " + req.params.articlename +"'" , function(err,result){
+        if(err){
+            res.status(500).send(err,toString());
+        }else{
+            if(result.rows.length===0){
+                res.status(404).send("Article not Found !");
+            }else{
+                var articleData = result.rows[0];
+                res.send(createTemplate(articleData));
+            }
+            
+        }
+        
+    });
     var articleName = req.params.articlename;
     res.send(createTemplate(articles[articleName]));
 });
