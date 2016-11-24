7ff8a15b3f410fe68085e187183352822730564c
melvin2016 <melvingeorge10@gmail.com>
2016-11-24 20:17:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -145,10 +145,10 @@ app.get('/articles/:articlename', function (req, res) {
         }else{
             /*if(result.rows.length===0){
                 res.status(404).send('Article not Found !');
-            }*/if{
+            }else{*/
                 var articleData = result.rows[0];
                 res.send(createTemplate(articleData));
-            }
+            //}
             
         }
         
