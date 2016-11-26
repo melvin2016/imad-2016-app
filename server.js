bfa5967237192cb0be3ab84eda1b375f453a020c
melvin2016 <melvingeorge10@gmail.com>
2016-11-26 23:47:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -175,73 +175,7 @@ app.post('/login',function(req,res){
                 var hashedP = hash(password,salt);
                 if(hashedP === dbString  ){
                     req.session.auth = {userId:result.rows[0].id};
-                    res.send([username,`function features(){
-        var button = document.getElementById('counter');
-        var counter=0;
-        button.onclick = function(){
-            
-            var request = new XMLHttpRequest();
-            request.onreadystatechange = function(){
-                
-                if(request.readyState===XMLHttpRequest.DONE){
-                    //We should do something
-                    if(request.status===200){
-                        var counter = request.responseText;
-                        span = document.getElementById('count');
-                        span.innerHTML = counter.toString();
-                        
-                    }
-                    
-                }
-                
-            };
-            request.open('GET','http://melvin2016.imad.hasura-app.io/counter',true);
-            request.send(null);
-        };
-    
-    
-    
-    var submit = document.getElementById('submit_btn');
-    var ul_list = document.getElementById('ul_list');
-    submit.onclick = function(){
-        
-        
-         
-        var request = new XMLHttpRequest();
-        request.onreadystatechange = function(){
-            
-            if(request.readyState===XMLHttpRequest.DONE){
-                //We should do something
-                if(request.status===200){
-                    var comments = request.responseText;
-                    
-                    
-                    comments = JSON.parse(comments);
-                    
-                    
-                    var list='';
-         
-                    for(i=0; i < comments.length ; i++){
-             
-                     list += '<li>'+comments[i]+'</li>';
-             
-                     }
-         
-                    ul_list.innerHTML = "<br>Comments:"+list;
-                    
-                }
-                
-            }
-            
-        };
-        var inputComment = document.getElementById('input_comment');
-        var comment = inputComment.value;
-        request.open('GET','http://melvin2016.imad.hasura-app.io/submit-comment'+currentArticleTitle,true);
-        request.send(null);
-         
-         
-     };
-  }`]);
+                    res.send(username);
                     
                   }else{
                      res.send(403).send('username/password is invalid !');
