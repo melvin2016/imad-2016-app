ade29ca0e49b1b104201356429e3fd262929b14d
melvin2016 <melvingeorge10@gmail.com>
2016-11-26 20:03:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -199,17 +199,16 @@ app.post('/login',function(req,res){
 });
 
 app.get('/check-login',function(req,res){
-    if (req.session && req.session.auth && req.session.auth.userId) {
-       // Load the user object
-       pool.query('SELECT * FROM "user" WHERE id = $1', [req.session.auth.userId], function (err, result) {
+   if(req.session && req.session.auth && req.session.auth.userId){
+       
+        pool.query('SELECT * FROM "user" WHERE id = $1', [req.session.auth.userId], function (err, result) {
            if (err) {
               res.status(500).send(err.toString());
            } else {
               res.send(result.rows[0].username);    
            }
-       }
-    
-    }
+        };
+   }
 });
 
 app.get('/logout',function(req,res){
