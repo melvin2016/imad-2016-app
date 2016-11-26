ed8f08f186f7daa02e1337b95a6c18454cdb704d
melvin2016 <melvingeorge10@gmail.com>
2016-11-26 23:49:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -84,7 +84,9 @@ function features(){
                         alert("User is Successfully Logged In !");
                         loginArea.innerHTML = '<div style="color:yellow"><bold>Hi <bold><i>'+request.responseText+'<i></div>';
                         logoutArea.innerHTML = '<a href="http://melvin2016.imad.hasura-app.io/logout"><button>Logout</button></a>';
-                        features();
+                        if(features()){
+                            location.reload(true);
+                        }
                         
                         
                         
