14b75bc961b5d5cc1bd257aed4e70e9ede1273b7
melvin2016 <melvingeorge10@gmail.com>
2016-11-27 00:58:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -171,6 +171,7 @@ var username = document.getElementById('username').value;
 var password = document.getElementById('password').value;
 if(username ||  password === ""){
     alert("Username Or Password Field Is Empty !");
+    break;
             
      }
     
