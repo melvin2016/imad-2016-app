565285e83fa5fcad575141624b318a0b832f1440
melvin2016 <melvingeorge10@gmail.com>
2016-11-26 19:56:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -66,70 +66,71 @@ submit.onclick = function(){
      
  };
  
- var submit_form = document.getElementById('submit_form');
- var logoutArea = document.getElementById('logoutArea');
- var loginArea = document.getElementById('loginArea');
- 
- submit_form.onclick = function login(){
-        var request = new XMLHttpRequest();
-        request.onreadystatechange = function(){
-            
-            if(request.readyState===XMLHttpRequest.DONE){
-                //We should do something
-                if(request.status===200){
-                    console.log("User is Successfully Logged In !");
-                    alert("User is Successfully Logged In !");
-                    loginArea.innerHTML = '<div style="color:yellow"><bold>Hi <bold><i>'+request.responseText+'<i></div>';
-                    logoutArea.innerHTML = '<a href="http://melvin2016.imad.hasura-app.io/logout"><button>Logout</button></a>';
-                    
-                    
-                    
-                    
-                }else if(request.status === 403){
-                    alert("Invalid username Or Password!");
-                }else if(request.status===500){
-                    alert("Something Went Wrong In The server ! ");
-                }
+ function loadLoginForm(){
+       var submit_form = document.getElementById('submit_form');
+     var logoutArea = document.getElementById('logoutArea');
+     var loginArea = document.getElementById('loginArea');
+     
+     submit_form.onclick = function login(){
+            var request = new XMLHttpRequest();
+            request.onreadystatechange = function(){
                 
+                if(request.readyState===XMLHttpRequest.DONE){
+                    //We should do something
+                    if(request.status===200){
+                        console.log("User is Successfully Logged In !");
+                        alert("User is Successfully Logged In !");
+                        loginArea.innerHTML = '<div style="color:yellow"><bold>Hi <bold><i>'+request.responseText+'<i></div>';
+                        logoutArea.innerHTML = '<a href="http://melvin2016.imad.hasura-app.io/logout"><button>Logout</button></a>';
+                        
+                        
+                        
+                        
+                    }else if(request.status === 403){
+                        alert("Invalid username Or Password!");
+                    }else if(request.status===500){
+                        alert("Something Went Wrong In The server ! ");
+                    }
+                    
+                }
+            };
+        var username = document.getElementById('username').value;
+        var password = document.getElementById('password').value;    
+    
+        request.open('POST','/login' , true);
+        request.setRequestHeader('Content-Type','application/json');
+        request.send(JSON.stringify({username:username,password:password}));
+        submit_form.value="Logging In...";
+     };
+     
+     var register_user = document.getElementById('register_user');
+     register_user.onclick = function register(){
+         var request = new XMLHttpRequest();
+            request.onreadystatechange = function(){
+                
+                if(request.readyState===XMLHttpRequest.DONE){
+                    //We should do something
+                    if(request.status===200){
+                        alert('User Succesfully Created !');
+                        register_user.value = 'Registered!';
+                        
+                }else{
+                    alert('Something Wrong With The Server !');
+                }
             }
         };
-    var username = document.getElementById('username').value;
-    var password = document.getElementById('password').value;    
-
-    request.open('POST','/login' , true);
-    request.setRequestHeader('Content-Type','application/json');
-    request.send(JSON.stringify({username:username,password:password}));
-    submit_form.value="Logging In...";
- };
- 
- var register_user = document.getElementById('register_user');
- register_user.onclick = function register(){
-     var request = new XMLHttpRequest();
-        request.onreadystatechange = function(){
-            
-            if(request.readyState===XMLHttpRequest.DONE){
-                //We should do something
-                if(request.status===200){
-                    alert('User Succesfully Created !');
-                    register_user.value = 'Registered!';
-                    
-            }else{
-                alert('Something Wrong With The Server !');
-            }
-        }
-    };
- var username = document.getElementById('username').value;
- var password = document.getElementById('password').value;
- request.open('POST','/create-user' , true);
- request.setRequestHeader('Content-Type','application/json');
- request.send(JSON.stringify({username:username,password:password}));
- register_user.value = "Registering...";
- 
+     var username = document.getElementById('username').value;
+     var password = document.getElementById('password').value;
+     request.open('POST','/create-user' , true);
+     request.setRequestHeader('Content-Type','application/json');
+     request.send(JSON.stringify({username:username,password:password}));
+     register_user.value = "Registering...";
      
-};
+         
+    };  
+     
+ }
+ 
+ 
 
 
-if(req.session && req.session.auth && req.session.auth.userId){
-    login();
-    
-}
