f69c9939ed0427854803fb279a03b796a1b4b4f8
melvin2016 <melvingeorge10@gmail.com>
2016-11-28 19:31:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -50,7 +50,10 @@ var register_user = document.getElementById('register_user');
                         loginArea.innerHTML = '<div style="color:green; font-size:15px;"><bold>Hi <bold><i>'+request.responseText+'<i></div>';
                         logoutArea.innerHTML = '<a href="http://melvin2016.imad.hasura-app.io/logout"><button>Logout</button></a>';
                         userAndPass.innerHTML="";
-                        loadComments();
+                        if(isLoggedIn()){
+                            loadComments();
+                        }
+                        
                          location.reload(true);
                          
                          
